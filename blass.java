42ef9f506a1b870d55ad1adf27e6ee5aecdc7511
Leonardo Mallet <leonardoms.2010@hotmail.com>
2024-03-21 12:16:59 -0300

    oi

--- blass.java
+++ blass.java
@@ -1,6 +1,9 @@
 public class blass {
     public static void main(String[] args) {
         System.out.println("Print");
+        int picles = 0;
+        System.out.println(picles);
+
     }
     
     
